64b189f1c096c0fe20546769d7061fed7ef8eeb1
amrakbari <amrakbaridev@gmail.com>
2025-03-07 22:45:22 +0330

    refactor: replaced layers/application/services/ with use_cases because of better structure

--- source/layers/application/use_cases.py
+++ source/layers/application/use_cases.py
@@ -3,13 +3,13 @@ from layers.core.repositories import IVideoRepository, IAudioRepository
 from layers.core.services import IAudioExtractorService
 
 
-class AudioExtractionService:
+class AudioExtractionUseCase:
     def __init__(self, video_repo: IVideoRepository, audio_repo: IAudioRepository, audio_extractor_service: IAudioExtractorService):
         self.video_repo = video_repo
         self.audio_repo = audio_repo
         self.audio_extractor_service = audio_extractor_service
 
-    def extract_audio(self, video_id: int) -> AudioEntity:
+    def execute(self, video_id: int) -> AudioEntity:
         video = self.video_repo.get_video(video_id)
         audio_path = self.audio_extractor_service.extract(video.path)
         audio = AudioEntity(
