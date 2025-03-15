16a307d0d730209e292aa29e7ab52a4258d42eeb
amrakbari <amrakbaridev@gmail.com>
2025-03-16 02:20:36 +0330

    fixed bug in admin panel

--- source/audio_extractor/models.py
+++ source/audio_extractor/models.py
@@ -15,11 +15,11 @@ class Video(BaseModel):
         (STATUS_ERROR, 'Error'),
     )
 
-    path = models.FilePathField()
+    path = models.FileField(upload_to='videos/')
     name = models.CharField(max_length=255, unique=True)
     audio_extraction_status = models.CharField(max_length=20, choices=AUDIO_EXTRACTION_STATUS_CHOICES, default=STATUS_PENDING)
 
 
 class Audio(BaseModel):
-    path = models.FilePathField()
+    path = models.FileField(upload_to='audios/')
     video = models.ForeignKey(Video, on_delete=models.SET_NULL, null=True)
--- source/config/urls.py
+++ source/config/urls.py
@@ -1,3 +1,6 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
@@ -13,3 +16,7 @@ urlpatterns = [
     # API endpoints
     path('api/', include('layers.presentation.urls')),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
